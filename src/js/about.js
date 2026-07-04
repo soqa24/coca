1630512643c2cfd6bb42b2a1b9c5bbf1cbe08f29
Test name <rabota1264@gmail.com>
2026-07-04 13:54:12 +0400

    Add page about, aboutUs section

---
+++ src/js/about.js
@@ -0,0 +1,12 @@
+import '/scss/about.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+// import {
+//   useInsightSlider,
+//   useTestimonialsSlider,
+// } from './components/home/slider.js';
+
+useTheme();
+useBurger();
