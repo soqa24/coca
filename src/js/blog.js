b859685289ed9e84a048164b2e67dc07f90316f3
Test name <rabota1264@gmail.com>
2026-07-10 17:28:22 +0400

    complete Blog page

--- src/js/blog.js
+++ src/js/blog.js
@@ -4,8 +4,10 @@ import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { initArcticlesSlider } from './components/blog/arcticles.js';
 import { filterBtn } from './components/blog/switchBtn.js';
+import { usePublicationsSlider } from './components/blog/publications.js';
 
 useTheme();
 useBurger();
 initArcticlesSlider();
 filterBtn();
+usePublicationsSlider();
