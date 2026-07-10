1a301f97e54ecca8d0d4d3b5144c2af1fff98d77
Test name <rabota1264@gmail.com>
2026-07-10 14:41:37 +0400

    add swection procrss, fix filter btn in articles section

--- src/js/blog.js
+++ src/js/blog.js
@@ -2,7 +2,10 @@ import '/scss/blog.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { initArcticlesSlider } from './components/blog/arcticles.js';
+import { filterBtn } from './components/blog/switchBtn.js';
 
 useTheme();
 useBurger();
-
+initArcticlesSlider();
+filterBtn();
